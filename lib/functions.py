c01353bc5f685bc3595f321576a3c533762849e0
ngotwaaron <ngotwaaron@gmail.com>
2024-05-31 08:11:00 +0300

    complete python p3 functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,21 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
+    print("Hello, programmer!")
     pass
 
-def greet(name):
+def greet(name="Sylvia"):
+    print(f"Hello, {name}!")
     pass
 
 def greet_with_default(name="programmer"):
+    print(f"Hello, {name}!")
     pass
 
-def add(num1, num2):
+def add(num1 = 45, num2 = 55):
+    return num1 + num2
     pass
 
-def halve(number):
+def halve(number = 100):
+    return number / 2
     pass
